63b6e414db84d98d2e260bc03dbeb11e166bcac3
Сергей <basov888@ya.ru>
2023-01-25 14:37:01 +0700

    New project HomeWork3 Ex03

---
+++ Ex03/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void CalculateСube(int number)
+{
+    int countNum = 1;
+    Console.Write("Таблица кубов чисел: ");
+    while (countNum <= number)
+    {
+        int result = countNum * countNum * countNum;
+        Console.Write(result);
+        if (countNum < number) 
+        {
+            Console.Write(", ");
+        }
+        countNum++;
+    }
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = GetNum("Введите натуральное число: ");
+
+CalculateСube(number);
