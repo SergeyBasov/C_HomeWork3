170f7838a24f104ef4d04f9f6c3b1148a7a0f350
Сергей <basov888@ya.ru>
2023-01-25 14:34:41 +0700

    New project HomeWork3 Ex01

---
+++ Ex01/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 19 Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+void PalindromCheck(int x)
+{
+    if (x / 10000 == x % 10 && (x % 10000) / 1000 == (x % 100) / 10)
+    {
+        Console.Write($"{x} да, это число палиндром");
+    }
+    else
+    {
+        Console.Write($"{x} нет, это число не палиндром");
+    }
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+int x = GetNum("Введите натуральное пятизначное число: ");
+
+PalindromCheck(x);
